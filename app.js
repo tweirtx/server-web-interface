9b29bc37b7a3d2ba5d1491c9e9682a4401b0b377
Travis Weir <tweirtx@gmail.com>
2018-11-23 15:22:10 -0600

    Listing works

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const http = require('http');
 const pm2 = require('pm2');
 const fs = require('fs');
+const shell = require('shell');
+var ref;
 
 function enumerate() {
-    var allFiles = fs.readdir('');
+    var processes = [];
+    fs.readdirSync('processes').forEach(file => {
+        processes.push(file);
+    });
+    var processObjs = [];
+    for (let process in processes) {
+        let pJSON = JSON.parse(fs.readFileSync('processes/' + processes[process]));
+        processObjs.push(pJSON.name);
+    }
+    return processObjs;
 }
 function restartProcess(procName) {
-    pm2.restart(procName);
+    stopProcess(procName);
+    startProcess(procName);
 }
 function stopProcess(procName) {
     pm2.stop(procName);
@@ -15,13 +27,21 @@ function startProcess(procName) {
     pm2.start(procName);
 }
 function gitPull(procDir) {
-    pm2.start("cd "+procDir+ " && git pull");
-    return true;
+    shell.Shell('cd ' + procDir + " && git pull");
 }
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var processes = enumerate();
+    for (let process in processes) {
+        ref = "<a href='/process?proc=" + processes[process] + "'>" + processes[process] + "</a> <br>";
+        res.write(ref);
+    }
+    res.end("Select a process to control");
 }).listen(8080);
+
+if (!fs.existsSync('processes')) {
+    fs.mkdirSync('processes');
+}
 
 console.log("Ready!");
